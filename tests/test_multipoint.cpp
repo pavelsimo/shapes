7fe18b5bcb3f13a31a04c08ac1f7cd178e5a1fd2
Pavel Simo <pavel.simo@gmail.com>
2019-01-26 03:45:25 -0800

    :white_check_mark: add tests

--- tests/test_multipoint.cpp
+++ tests/test_multipoint.cpp
@@ -13,12 +13,16 @@ TEST_CASE("MultiPoint tests")
         CHECK(mp.geom_type_str() == "MULTIPOINT");
     }
 
-    SECTION("2d multipoint constructor - initializer list")
+    SECTION("2d multipoint constructor - initializer list <double>")
     {
-        MultiPoint mp2 = {{1.0, 2.0}, {2.0, 3.0}};
+        MultiPoint mp = {{1.0, 2.0}, {2.0, 3.0}};
+        CHECK(mp[0].x == 1.0);
+        CHECK(mp[0].y == 2.0);
+        CHECK(mp[1].x == 2.0);
+        CHECK(mp[1].y == 3.0);
     }
 
-    SECTION("3d multipoint constructor - initializer list")
+    SECTION("3d multipoint constructor - initializer list <double>")
     {
         MultiPoint mp = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
         CHECK(mp[0].x == 1.0);
@@ -27,5 +31,66 @@ TEST_CASE("MultiPoint tests")
         CHECK(mp[1].x == 4.0);
         CHECK(mp[1].y == 5.0);
         CHECK(mp[1].z == 6.0);
+    }
+
+    SECTION("2d multipoint envelope")
+    {
+        MultiPoint mp = {{1.0, 2.0}, {3.0, 4.0}};
+        CHECK(mp.envelope().max().x == 3.0);
+        CHECK(mp.envelope().max().y == 4.0);
+        CHECK(mp.envelope().min().x == 1.0);
+        CHECK(mp.envelope().min().y == 2.0);
+    }
+
+    SECTION("2d multipoint for each")
+    {
+        auto points = MultiPoint{{1.0, 2.0}, {3.0, 4.0}, {5.0, 6.0}};
+        int count = 0;
+        for (const auto& point: points)
+        {
+            CHECK(point.x != 0);
+            CHECK(point.y != 0);
+            ++count;
+        }
+        CHECK(count == 3);
+    }
+
+    SECTION("3d multipoint for each")
+    {
+        auto points = MultiPoint{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
+        int count = 0;
+        for (const auto& point: points)
+        {
+            CHECK(point.x != 0);
+            CHECK(point.y != 0);
+            CHECK(point.z != 0);
+            ++count;
+        }
+        CHECK(count == 3);
+    }
+
+    SECTION("2d multipoint index operator")
+    {
+        auto points = MultiPoint{{1.0, 2.0}, {3.0, 4.0}};
+        auto p1 = points[0];
+        auto p2 = points[1];
+        CHECK(p1.x == 1.0);
+        CHECK(p1.y == 2.0);
+        CHECK(p2.x == 3.0);
+        CHECK(p2.y == 4.0);
+    }
+
+    SECTION("3d multipoint index operator")
+    {
+        auto points = MultiPoint{{1.0, 2.0}, {3.0, 4.0}, {5.0, 6.0}};
+        auto p1 = points[0];
+        auto p2 = points[1];
+        auto p3 = points[2];
+        CHECK(p1.x == 1.0);
+        CHECK(p1.y == 2.0);
+        CHECK(p2.x == 3.0);
+        CHECK(p2.y == 4.0);
+        CHECK(p3.x == 5.0);
+        CHECK(p3.y == 6.0);
     }
 }
--- tests/test_point.cpp
+++ tests/test_point.cpp
@@ -4,25 +4,21 @@
 
 using namespace simo::shapes;
 
-TEST_CASE("Point tests")
-{
-    SECTION("geometry type")
-    {
+TEST_CASE("Point tests") {
+    SECTION("geometry type") {
         Point p;
         CHECK(p.geom_type() == GeometryType::POINT);
         CHECK(p.geom_type_str() == "POINT");
     }
 
-    SECTION("empty constructor")
-    {
+    SECTION("empty constructor") {
         Point p;
         CHECK(p.x == 0);
         CHECK(p.y == 0);
         CHECK(p.dimension() == 2);
     }
 
-    SECTION("2d point constructor - initializer list")
-    {
+    SECTION("2d point constructor - initializer list") {
         Point p = {1, 2};
         CHECK(p.x == 1);
         CHECK(p.y == 2);
@@ -30,13 +26,38 @@ TEST_CASE("Point tests")
         CHECK(p.dimension() == 2);
     }
 
-
-    SECTION("3d point constructor - initializer list")
-    {
+    SECTION("3d point constructor - initializer list") {
         Point p = {1, 2, 3};
         CHECK(p.x == 1);
         CHECK(p.y == 2);
         CHECK(p.z == 3);
         CHECK(p.dimension() == 3);
+    }
+
+    SECTION("2d point from geojson") {
+        auto p = Point::from_json("{\"type\": \"Point\", \"coordinates\": [1.0, 2.0]}");
+        CHECK(p.x == 1.0);
+        CHECK(p.y == 2.0);
+        CHECK(p.dimension() == 2);
+        CHECK(p.geom_type_str() == "POINT");
+    }
+
+    SECTION("3d point from geojson") {
+        auto p = Point::from_json("{\"type\": \"Point\", \"coordinates\": [1.0, 2.0, 3.0]}");
+        CHECK(p.x == 1.0);
+        CHECK(p.y == 2.0);
+        CHECK(p.z == 3.0);
+        CHECK(p.dimension() == 3);
+        CHECK(p.geom_type_str() == "POINT");
+    }
+
+    SECTION("2d point to geojson") {
+        auto p = Point{1, 2};
+        CHECK(p.to_json() == "{\"coordinates\":[1.0,2.0],\"type\":\"Point\"}");
+    }
+
+    SECTION("3d point to geojson") {
+        auto p = Point{1, 2, 3};
+        CHECK(p.to_json() == "{\"coordinates\":[1.0,2.0,3.0],\"type\":\"Point\"}");
     }
 }
